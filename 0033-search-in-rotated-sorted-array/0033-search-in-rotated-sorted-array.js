53598e5180802924b248a124ff51cfb96bf664bd
LilaInCode <123276066+LilaInCode@users.noreply.github.com>
2023-10-25 10:14:57 +1100

    Time: 52 ms (57.67%), Space: 42 MB (48.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,5 +4,32 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  return nums.indexOf(target)
+    let low = 0, high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        // Determine if the left half is sorted
+        if (nums[low] <= nums[mid]) {
+            // If target lies in the left half's range, adjust 'high' to 'mid - 1'
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            // If target lies in the right half's range, adjust 'low' to 'mid + 1'
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+
+
 };
